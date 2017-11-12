089d50accae9cfbaa10ce4ecf89cf9c02dbf6b79
Ashes17b <Ashes17b@gmail.com>
2017-11-12 22:58:16 +0000

    Client.java/feature: Request handler

--- src/Client.java
+++ src/Client.java
@@ -9,13 +9,31 @@ public class Client {
         try {
             Socket socket = new Socket(InetAddress.getLocalHost(), port);
 
-            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-            String command = "sudo pwd";
-            os.writeObject(command);
-            answer = (String) is.readObject();
-            System.out.println(answer);
-            socket.close();
+            while (true) {
+                ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+                String command = "sudo pwd";
+                os.writeObject(command);
+                answer = (String) is.readObject();
+                String result = answer.substring(0, 2);
+                /** Success */
+                if (result == "10") {
+                    System.out.println(answer.substring(6));
+                    System.out.println("Ok");
+                }
+                /** Client Error */
+                else if (result == "20")
+                    System.err.println("Bad request");
+                else if (result == "21")
+                    System.err.println("Unauthorized");
+                else if (result == "22")
+                    System.err.println("Not Found");
+                /** Server Error */
+                else if (result == "38")
+                    System.err.println("Internal Server Error");
+
+                socket.close();
+            }
         }
         catch (UnknownHostException e){
             System.out.println("Address no responsable.");
