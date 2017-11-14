ad1aba20932f7d9514d7345843529e8a50291e72
Ashes17b <Ashes17b@gmail.com>
2017-11-14 00:03:27 +0000

    Client.java/feature: Request handler

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,10 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Client {
     private static int port = 49001;
-    static String answer;
+    private static ArrayList<String> answer;
 
     public static void main(String[] args){
         try {
@@ -12,28 +13,15 @@ public class Client {
             while (true) {
                 ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
                 ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
-                String command = "sudo pwd";
+                Scanner sc = new Scanner(System.in);
+                String command = sc.nextLine();
                 os.writeObject(command);
-                answer = (String) is.readObject();
-                String result = answer.substring(0, 2);
-                /** Success */
-                if (result == "10") {
-                    System.out.println(answer.substring(6));
-                    System.out.println("Ok");
-                }
-                /** Client Error */
-                else if (result == "20")
-                    System.err.println("Bad request");
-                else if (result == "21")
-                    System.err.println("Unauthorized");
-                else if (result == "22")
-                    System.err.println("Not Found");
-                /** Server Error */
-                else if (result == "38")
-                    System.err.println("Internal Server Error");
-
-                socket.close();
+                answer = (ArrayList<String>) is.readObject();
+                if (!treatment(answer))
+                    break;
             }
+
+            socket.close();
         }
         catch (UnknownHostException e){
             System.out.println("Address no responsable.");
@@ -43,12 +31,36 @@ public class Client {
             System.out.println("Exception on IO.");
             e.printStackTrace();
         }
-        catch (ClassNotFoundException e){
+        catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-//        catch (InterruptedException e){
-//            System.out.println("Exception on thread.");
-//            e.printStackTrace();
-//        }
+    }
+
+    public static boolean treatment(ArrayList<String> answer) {
+        for (String s: answer) {
+            String result = s.substring(0, 2);
+
+            /** Success */
+            if (Objects.equals(result, "10")) {
+                System.out.println(s.substring(6));
+                System.out.println("Ok");
+            }
+            /** Client Error */
+            else if (Objects.equals(result, "20"))
+                System.err.println("Bad request");
+            else if (Objects.equals(result, "21")) {
+                System.err.println("Unauthorized");
+                System.out.print("Input password: ");
+            }
+            else if (Objects.equals(result, "22"))
+                System.err.println("Not Found");
+            /** Server Error */
+            else if (Objects.equals(result, "38")) {
+                System.err.println("Internal Server Error");
+                return false;
+            }
+        }
+
+        return true;
     }
 }
